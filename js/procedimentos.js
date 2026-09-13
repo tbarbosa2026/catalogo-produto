2ba8937bd13c261963269985a0646a175106739d
tbarbosa2026 <tiagobarbosa1008@gmail.com>
2026-09-12 22:19:49 -0300

    Add files via upload

---
+++ js/procedimentos.js
@@ -0,0 +1,128 @@
+const procedimentos = [
+
+    {
+        nome: "Calibração Balança Texpan",
+        arquivo: "procedimentos/01_Calibracao_Balanca_Texpan.pdf"
+    },
+
+    {
+        nome: "Calibrar Transdutor de Pressão - Prensa MDF2",
+        arquivo: "procedimentos/02_Calibrar_Transdutor_de_Pressao_-_Prensa_MDF2.pdf"
+    },
+
+    {
+        nome: "Despressurização hidraulico Prensa MDF2",
+        arquivo: "procedimentos/03_Despressurizacao_hidraulico_Prensa_MDF2.pdf"
+    },
+
+    {
+        nome: "Diagnostico de falha ServoValvula",
+        arquivo: "procedimentos/04_Diagnostico_de_falha_ServoValvula.pdf"
+    },
+
+    {
+        nome: "DIAGNÓSTICO DE FALHAS INVERSOR SEW",
+        arquivo: "procedimentos/05_DIAGNOSTICO_DE_FALHAS_INVERSOR_SEW.pdf"
+    },
+
+    {
+        nome: "Diagnóstico Falha intermitente SPC - PRENSA MDF2 (1)",
+        arquivo: "procedimentos/06_Diagnostico_Falha_intermitente_SPC_-_PRENSA_MDF2_1.pdf"
+    },
+
+    {
+        nome: "MEDIDOR DE VAZÃO ECORESINATOR",
+        arquivo: "procedimentos/07_MEDIDOR_DE_VAZAO_ECORESINATOR.pdf"
+    },
+
+    {
+        nome: "Procedimento de calibração com dispositivo PDF",
+        arquivo: "procedimentos/08_Procedimento_de_calibracao_com_dispositivo_PDF.pdf"
+    },
+    
+    {
+        nome: "Procedimento de Configuração Calibração Sensor de Nível Radioativo",
+        arquivo: "procedimentos/10_Procedimento_de_Configuracao_Calibracao_Sensor_de_Nivel_Radioativo.pdf"
+    },
+
+    {
+        nome: "Procedimento Diagnóstico major fault RSlogix 5000",
+        arquivo: "procedimentos/11_Procedimento_Diagnostico_major_fault_RSlogix_5000.pdf"
+    },
+
+    {
+        nome: "Procedimento e Diagnóstico e Reset CLP Sick NR12- Pátio",
+        arquivo: "procedimentos/12_Procedimento_e_Diagnostico_e_Reset_CLP_Sick_NR12-_Patio.pdf"
+    },
+
+    {
+        nome: "Procedimento leitura EX8000",
+        arquivo: "procedimentos/13_Procedimento_leitura_EX8000.pdf"
+    },
+
+    {
+        nome: "Procedimento Lógica do Picador",
+        arquivo: "procedimentos/14_Procedimento_Logica_do_Picador.pdf"
+    },
+
+    {
+        nome: "procedimento medidor de oxigênio lambda",
+        arquivo: "procedimentos/15_procedimento_medidor_de_oxigenio_lambda.pdf"
+    },
+
+    {
+        nome: "PROCEDIMENTO PARA PARTIDA DO STENOGRAPH",
+        arquivo: "procedimentos/16_PROCEDIMENTO_PARA_PARTIDA_DO_STENOGRAPH.pdf"
+    },
+
+    {
+        nome: "Procedimento reset da CPU na central do Minimax (1)",
+        arquivo: "procedimentos/17_Procedimento_reset_da_CPU_na_central_do_Minimax_1.pdf"
+    },
+
+    {
+        nome: "PROCEDIMENTO TESTE CORPO DE PROVAS DETECTOR METAL",
+        arquivo: "procedimentos/18_PROCEDIMENTO_TESTE_CORPO_DE_PROVAS_DETECTOR_METAL.pdf"
+    },
+
+    {
+        nome: "procedimento videoJet 3340",
+        arquivo: "procedimentos/19_procedimento_videoJet_3340.pdf"
+    },
+
+    {
+        nome: "RESET DE FALHAS MÓDULOS SPC - PRENSA MDF2",
+        arquivo: "procedimentos/20_RESET_DE_FALHAS_MODULOS_SPC_-_PRENSA_MDF2.pdf"
+    },
+
+    {
+        nome: "Simulação de corte das Serras Diagonais",
+        arquivo: "procedimentos/21_Simulacao_de_corte_das_Serras_Diagonais.pdf"
+    },
+
+    {
+        nome: "Teste Termopar",
+        arquivo: "procedimentos/22_Teste_Termopar.pdf"
+    },
+
+    {
+        nome: "Teste Transmissor - Presys Isocal MCS-12",
+        arquivo: "procedimentos/23_Teste_Transmissor_-_Presys_Isocal_MCS-12.pdf"
+    },
+
+    {
+        nome: "Troca cartão SPC- Reset transdutores",
+        arquivo: "procedimentos/24_Troca_cartao_SPC-_Reset_transdutores.pdf"
+    },
+
+    {
+        nome: "TROCA DE TRANSDUTOR DE DISTANCIA - PRENSA MDF2",
+        arquivo: "procedimentos/25_TROCA_DE_TRANSDUTOR_DE_DISTANCIA_-_PRENSA_MDF2.pdf"
+    },
+
+    {
+        nome: "TROCA DO RELÉ SIEMENS 3TK2842 PARA 3SK1122-1",
+        arquivo: "procedimentos/26_TROCA_DO_RELE_SIEMENS_3TK2842_PARA_3SK1122-1.pdf"
+    }
+
+];
